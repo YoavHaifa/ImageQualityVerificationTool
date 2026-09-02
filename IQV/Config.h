3bd88ae108cf0b6ad6157598fa4373ed25640d55
Yoav Bar <yoav.bar.haifa@gmail.com>
2026-09-02 13:49:33 +0300

    Close version 0.8.5
    Bump version, update user guide with the new low-resolution-area
    config setting and the case-version-mismatch message.

--- IQV/Config.h
+++ IQV/Config.h
@@ -79,7 +79,7 @@ public:
 
 	// Bump this when a change is expected to affect scoring results. Used e.g. to name
 	// baseline result snapshots ("<msLogRoot>_<msVersion>") for regression comparison.
-	std::string msVersion = "0.8.4";
+	std::string msVersion = "0.8.5";
 
 	std::string msLogRoot = "d:\\IQV_Log";
 	std::string msCaseLogDir; // <msLogRoot>\[<batch root name>\]<current case name>[_<miCaseIndex>], set by SetCurrentCase
